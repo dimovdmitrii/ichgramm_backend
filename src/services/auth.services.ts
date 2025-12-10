0143e0669b943dafd834c36987be3be7103ad97d
dimovdmitrii <dimovdima1509@gmail.com>
2025-12-10 09:43:31 +0100

    lifetime accesstoken

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -26,8 +26,8 @@ export interface LoginResult {
 
 export const createTokens = (id: Types.ObjectId) => {
   const idString = id.toString();
-  const accessToken = generateToken({ id: idString }, { expiresIn: "15s" });
-  const refreshToken = generateToken({ id: idString }, { expiresIn: "17s" });
+  const accessToken = generateToken({ id: idString }, { expiresIn: "15m" });
+  const refreshToken = generateToken({ id: idString }, { expiresIn: "15d" });
   return { accessToken, refreshToken };
 };
 
